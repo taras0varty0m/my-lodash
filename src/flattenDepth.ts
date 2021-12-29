4f8c2d850f7290d2dc7a7146c72759fbade33c09
Artyom Tarasov <tarasovar457@gmail.com>
2021-12-29 20:05:02 +0600

    change let to const in flattenDepth

--- src/flattenDepth.ts
+++ src/flattenDepth.ts
@@ -1,4 +1,4 @@
-let arr: any[] = [];
+const arr: any[] = [];
 const flattenDepth = (array: any[], depth = 1) => {
   array.forEach((element: any) => {
     Array.isArray(element) && depth !== 0 ? flattenDepth(element, depth - 1) : arr.push(element);
